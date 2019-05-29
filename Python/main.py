3a701373e5e7d67e00b0472562f26fccb8111b5f
LiorRaines <liorraines@gmail.com>
2019-05-29 14:58:22 +0300

    esadf

---
+++ Python/main.py
@@ -0,0 +1,12 @@
+from BlockChainParser import *
+from DB import *
+
+
+def main():
+    #parser = BlockChainParser()
+    #parser.parse()
+    db_client = DBClient()
+
+
+if __name__ == "__main__":
+    main()
