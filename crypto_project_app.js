4d038ea8438ffae0b8314c2ebaf39d4c319f887f
Emri Biran <45913023+EmriBiran@users.noreply.github.com>
2019-05-29 15:00:57 +0300

    csdvc

--- crypto_project_app.js
+++ crypto_project_app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var path = require("path");
 var mysql = require('mysql');
+var builder = require('xmlbuilder');
+var fs = require('fs');
 
 app.use(express.static(path.join(__dirname, "public")));
-document.getElementsByClassName("tablink")[0].click();
-
 
 function read_from_DB(address)
 {
@@ -27,82 +27,38 @@ function read_from_DB(address)
     });
 }
 
-function ReadMore() {
-	var dots = document.getElementById("dots");
-	var moreText = document.getElementById("more");
-	var btnText = document.getElementById("myBtn");
-	
-	if (dots.style.display === "none") {
-		dots.style.display = "inline";
-		btnText.innerHTML = "Read more"; 
-		moreText.style.display = "none";
-	} else {
-		dots.style.display = "none";
-		btnText.innerHTML = "Read less"; 
-		moreText.style.display = "inline";
-	}
-}
-		
-function openLink(evt, linkName) 
+function make_xml_file(address)
 {
-	var i, x, tablinks;
-	x = document.getElementsByClassName("myLink");
-	for (i = 0; i < x.length; i++) 
-		x[i].style.display = "none";
-	tablinks = document.getElementsByClassName("tablink");
-	for (i = 0; i < x.length; i++) 
-		tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-	document.getElementById(linkName).style.display = "block";
-	evt.currentTarget.className += " w3-red";
-}
+    var xml = builder.create('coinBook');
+    xml.ele('coin')
+        .ele('coinName', "Some_coin").up()
+        .ele('coinBalance', '52').up()
+        .up()
+    .end();
 
-function validMail(){
-	var frmvalidator  = new Validator("contactform");
-	frmvalidator.addValidation("name","req","Please provide your name");
-	frmvalidator.addValidation("email","req","Please provide your email");
-	frmvalidator.addValidation("email","email", "Please enter a valid email address");
-}
 
-function loadDoc() 
-{
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function()
-	{
-		if (this.readyState == 4 && this.status == 200)
-		{
-			myFunction(this);
-		}
-	};
-	xhttp.open("GET", "cd_catalog.xml", true);
-	xhttp.send();
-}
-
-function myFunction(xml)
-{
-	var i;
-	var xmlDoc = xml.responseXML;
-	var table="<tr><th>Artist</th><th>Title</th></tr>";
-	var x = xmlDoc.getElementsByTagName("CD");
-	for (i = 0; i <x.length; i++) 
-	{ 
-		table += "<tr><td>" +
-		x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-		"</td><td>" +
-		x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-		"</td></tr>";
-	}
-	document.getElementById("demo").innerHTML = table;
+    var doc = xml.toString({pretty: true});
+    fs.writeFile('C:\\temp\\test.xml', doc, function(err){
+        if(err)
+        {
+            return console.log(err);
+        }
+        console.log("saved xml");
+    });
 }
 
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "public", "MainPage.html"));
-})
+app.get("/", function(req, res){  // the main page
+    res.sendFile(path.join(__dirname, "public\\HTML", "MainPage.html"));
+});
 
-read_from_DB("Lior");
+app.get("/EXMPLExml.xml", function(req, res){  // sending the xml file as response for the xml request
+    res.contentType('application/xml');
+    res.sendFile(path.join(__dirname, "public\\HTML", "EXMPLExml.xml"));   
+});
+
+read_from_DB("lior");
 app.listen(80);
-
-
 
 
 
